1d336578ea4f40993475e61d98b7d4c748b088c7
sys-arthur <arthurern48@gmail.com>
2021-12-21 17:59:06 -0300

    db infos

--- APIGetExamples/Program.cs
+++ APIGetExamples/Program.cs
@@ -8,27 +8,37 @@ var app = builder.Build();
 app.MapPost("/product", (Product product) =>
 {
 	ProductRepo.addProduct(product);
-	return "Produto salvo com sucesso";
+	return Results.Created("OK", "Usuário criado com sucesso!");
 });
 
 app.MapGet("/product/{code}", ([FromRoute] string code) =>
 {
 	var product = ProductRepo.getProduct(code);
-	return product;
+	if (product != null)
+	{
+		System.Console.WriteLine($"User: {product.Name}");
+		return Results.Ok(product);
+	}
+	return Results.NotFound();
 });
 
 app.MapPut("/product", (Product product) =>
 {
-	var productSaved = ProductRepo.getProduct(product.Code ?? "");
-	productSaved.Name = product.Name;
-	return "Produto editado com sucesso!";
+	var productSaved = ProductRepo.getProduct(product.Code ?? " ");
+	productSaved!.Name = product.Name;
+	return "Usuário editado com sucesso!";
 });
 
 app.MapDelete("/product/{code}", ([FromRoute] string code) =>
 {
 	var product = ProductRepo.getProduct(code);
-	ProductRepo.removeProduct(product);
-	return "Produto deletado com sucesso!";
+	ProductRepo.removeProduct(product!);
+	return Results.Ok($"Usuário '{product?.Name}' deletado com sucesso");
+});
+
+app.MapGet("/configuration/database", (IConfiguration configuration) =>
+{
+	return Results.Ok(configuration["database:connection"]);
 });
 
 app.Run();
@@ -46,9 +56,9 @@ public static class ProductRepo
 		Products.Add(product);
 	}
 
-	public static Product getProduct(string code)
+	public static Product? getProduct(string code)
 	{
-		return Products.First(product => product.Code == code);
+		return Products?.First(product => product.Code == code);
 	}
 
 	public static void removeProduct(Product product)
