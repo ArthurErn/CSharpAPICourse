309d9244a81d184186325d22df8651ce20c6d012
sys-arthur <arthurern48@gmail.com>
2021-12-21 15:13:51 -0300

    API CACHE STORAGE

--- APIGetExamples/Program.cs
+++ APIGetExamples/Program.cs
@@ -1,45 +1,71 @@
+//ATUALIZADO
+//SALVANDO E RETORNANDO JSON, ARMAZENADOS NO CACHE
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 //METODO GET BASICO
-app.MapGet("/", () => "Hello World!");
+
+// app.MapGet("/", () => "Hello World!");
+
 
 //ADICIONAR HEADERS NA ROTA GET
-app.MapGet("/AddHeader", (HttpResponse response) =>
-{
-	response.Headers.Add("header", "Arthur ern");
-	return "Olá, mundo";
-});
+
+// app.MapGet("/AddHeader", (HttpResponse response) =>
+// {
+// 	response.Headers.Add("header", "Arthur ern");
+// 	return "Olá, mundo";
+// });
+
+
+
+
+
+//PEGANDO OS DADOS POR QUERY E RETORNANDO
+//EX: localhost:3001/getproduct?datainicial=26/12/2015&datafinal=21/07/2019
+
+// app.MapGet("/getproduct", ([FromQuery] string dataInicial, [FromQuery] string dataFinal) =>
+// {
+// 	return dataInicial + " - " + dataFinal;
+// });
+
 
 //POSTAR UMA ENTIDADE E RETORNA-LA
 app.MapPost("/saveproduct", (Product product) =>
 {
-	return product.Code + " - " + product.Name;
-});
-
-//PEGANDO OS DADOS POR QUERY E RETORNANDO
-//EX: localhost:3001/getproduct?datainicial=26/12/2015&datafinal=21/07/2019
-app.MapGet("/getproduct", ([FromQuery] string dataInicial, [FromQuery] string dataFinal) =>
-{
-	return dataInicial + " - " + dataFinal;
+	ProductRepo.addProduct(product);
+	return "Salvo com sucesso";
 });
 
 //INFORMAÇÃO ESPECIFICADA DIRETAMENTE NA ROTA
 app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
 {
-	return code;
-});
-
-//RETORNA INFORMAÇÃO ADICIONADA NO HEADER
-app.MapGet("/getproductheader", (HttpRequest request) =>
-{
-	return request.Headers["product-code"].ToString();
+	var product = ProductRepo.getProduct(code);
+	return product;
 });
 
 
 app.Run();
+
+public static class ProductRepo
+{
+	public static List<Product> Products { get; set; }
+
+	public static void addProduct(Product product)
+	{
+		if (Products == null)
+		{
+			Products = new List<Product>();
+		}
+		Products.Add(product);
+	}
+
+	public static Product getProduct(string code)
+	{
+		return Products.Last(product => product.Code == code);
+	}
+}
 
 public class Product
 {
