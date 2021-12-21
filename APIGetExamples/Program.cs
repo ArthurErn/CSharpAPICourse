14c459e5b226632d56b12a730945081b2a18480c
sys-arthur <arthurern48@gmail.com>
2021-12-21 15:45:50 -0300

    CRUD in cache

--- APIGetExamples/Program.cs
+++ APIGetExamples/Program.cs
@@ -5,26 +5,37 @@ using Microsoft.AspNetCore.Mvc;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//POSTAR UMA ENTIDADE E RETORNA-LA
-app.MapPost("/saveproduct", (Product product) =>
+app.MapPost("/product", (Product product) =>
 {
 	ProductRepo.addProduct(product);
-	return "Salvo com sucesso";
+	return "Produto salvo com sucesso";
 });
 
-//INFORMAÇÃO ESPECIFICADA DIRETAMENTE NA ROTA
-app.MapGet("/getproduct/{code}", ([FromRoute] string code) =>
+app.MapGet("/product/{code}", ([FromRoute] string code) =>
 {
 	var product = ProductRepo.getProduct(code);
 	return product;
 });
 
+app.MapPut("/product", (Product product) =>
+{
+	var productSaved = ProductRepo.getProduct(product.Code ?? "");
+	productSaved.Name = product.Name;
+	return "Produto editado com sucesso!";
+});
+
+app.MapDelete("/product/{code}", ([FromRoute] string code) =>
+{
+	var product = ProductRepo.getProduct(code);
+	ProductRepo.removeProduct(product);
+	return "Produto deletado com sucesso!";
+});
 
 app.Run();
 
 public static class ProductRepo
 {
-	public static List<Product> Products { get; set; }
+	public static List<Product>? Products { get; set; }
 
 	public static void addProduct(Product product)
 	{
@@ -37,7 +48,12 @@ public static class ProductRepo
 
 	public static Product getProduct(string code)
 	{
-		return Products.Last(product => product.Code == code);
+		return Products.First(product => product.Code == code);
+	}
+
+	public static void removeProduct(Product product)
+	{
+		Products?.Remove(product);
 	}
 }
 
